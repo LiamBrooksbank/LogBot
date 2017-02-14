d92603b853ade23bbd0e9e609fd825fb2cbaf143
LiamB <liam.brooksbank@gmail.com>
2017-02-14 23:57:32 +0200

    Started work on log structure

--- LogBot/Program.cs
+++ LogBot/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
+using MargieBot;
+using System.Text;
 
 namespace LogBot
 {
@@ -9,6 +12,37 @@ namespace LogBot
     {
         public static void Main(string[] args)
         {
+
+            var MyBot = new Bot();
+
+            // Get Token (encapsulate elsewhere later)
+            string Token = File.ReadAllText("LogBotInfo.txt");
+
+            MyBot.Responders.Add(new Greeting());
+
+            MyBot.Connect(Token);
+            MyBot.Aliases = new List<String>() { "LogBot", "logbot" };
+
+            while (Console.ReadLine() != "close") ;
         }
+    }
+
+    /// <summary>
+    /// Describes a single session of work on a single project
+    /// </summary>
+    public class Log
+    {
+        private Project project;
+        private int time; // in hours
+        private string description;
+    }
+
+    /// <summary>
+    /// A specific project
+    /// </summary>
+    public class Project
+    {
+        private string name;
+        private string description;
     }
 }
---
+++ LogBot/Responders.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MargieBot;
+using System.Text;
+
+namespace LogBot
+{
+    public class Greeting : IResponder
+    {
+        public bool CanRespond(ResponseContext context)
+        {
+            return context.Message.Text.ToLower().Contains("hey")
+                && context.Message.MentionsBot;
+        }
+
+        public BotMessage GetResponse(ResponseContext context)
+        {
+            var builder = new StringBuilder();
+            builder.Append("Hey! ").Append(context.Message.User.FormattedUserID);
+            return new BotMessage() { Text = builder.ToString() };
+        }
+    }
+}
